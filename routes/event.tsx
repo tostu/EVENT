187a3451dd51be52ab5af5641e1f5549e6ed5935
Torge Stubbe <tostu@LUFT.local>
2025-08-03 23:59:36 +0200

    update event layout

--- routes/event.tsx
+++ routes/event.tsx
@@ -40,52 +40,20 @@ export default function EventsPage({ data }: PageProps<Data>) {
         {events.map((event) => (
           <li class="list-row" key={event.id}>
             <div>
-              <img
-                class="size-10 rounded-box"
-                src="https://img.daisyui.com/images/profile/demo/1@94.webp"
-              />
-            </div>
-            <div>
               <div>{event.title}</div>
               <div class="text-xs uppercase font-semibold opacity-60">
-                {event.description}
+                {event.date.length === 1
+                  ? event.date[0]?.toLocaleDateString()
+                  : event.date[0]?.toLocaleDateString() + " - " +
+                    event.date[event.date.length - 1]?.toLocaleDateString()}
+              </div>
+              <div class="text-xs uppercase font-semibold opacity-60">
+                {event.time || ""}
+              </div>
+              <div class="text-xs uppercase font-semibold opacity-60">
+                {event.location || ""}
               </div>
             </div>
-            <button class="btn btn-square btn-ghost">
-              <svg
-                class="size-[1.2em]"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <g
-                  stroke-linejoin="round"
-                  stroke-linecap="round"
-                  stroke-width="2"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <path d="M6 3L20 12 6 21 6 3z"></path>
-                </g>
-              </svg>
-            </button>
-            <button class="btn btn-square btn-ghost">
-              <svg
-                class="size-[1.2em]"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <g
-                  stroke-linejoin="round"
-                  stroke-linecap="round"
-                  stroke-width="2"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z">
-                  </path>
-                </g>
-              </svg>
-            </button>
           </li>
         ))}
       </ul>
